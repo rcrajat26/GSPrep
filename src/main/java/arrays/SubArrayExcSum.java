271357a3e6bb655d5e3b9ffc035aa570c4ad6afe
Rajat Chikkodikar <rajat_chikkodikar@epam.com>
2021-06-23 15:37:11 +0530

    commit

---
+++ src/main/java/arrays/SubArrayExcSum.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class SubArrayExcSum {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/mathematics/VulgarToDecimal.java
+++ src/main/java/mathematics/VulgarToDecimal.java
@@ -1,23 +1,77 @@
 package mathematics;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class VulgarToDecimal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        String res = vulg2deci(4l,7l);
+        File input = new File("C:\\Users\\Rajat_Chikkodikar\\Desktop\\My files\\Projects\\GSIncubation\\src\\main\\java\\mathematics\\scratch.txt");
+        Scanner sc = new Scanner(input);
+
+        double num = sc.nextDouble();
+        double denom = sc.nextDouble();
+
+        String res = vulg2deci(num,denom);
         System.out.println(res);
     }
 
     private static String vulg2deci(double num, double denom) {
 
-        Double res =  num/denom;
-        String s = res.toString();
+        Map<Integer,Integer> mp = new LinkedHashMap<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("0.");
+        int resCount=0,containsCnt=0,tmp=(int)num;
+        boolean vulgar=false;
+        while(num%denom!=0) {
+            if(num<denom) {
+                num*=10;
+                while (num<denom) {
+                    sb.append("0");
+                    num*=10;
+                }
+            }
+            int rem = (int) ((int)num/denom);
+            if(!mp.containsKey(rem)) {
+                mp.put(rem,1);
+                resCount++;
+            } else {
+                if(mp.get(rem) == 2)
+                {
+                    vulgar=true;
+                    break;
+                }
+                mp.put(rem,mp.get(rem)+1);
+                containsCnt++;
+            }
+            num = num%denom;
+            if(resCount == containsCnt && mp.size() == resCount) {
+                vulgar =true;
+                break;
+            }
+        }
 
-        String[] arr = s.split("\\.");
-        s= arr[1];
+        StringBuilder vul = new StringBuilder();
+        if(vulgar) {
+            vul.append("(");
+            mp.entrySet().forEach(e->{
+                if(e.getValue()%2 == 0)
+                vul.append(e.getKey());
+                else
+                    sb.append(e.getKey());
+            });
+            vul.append(")");
+            sb.append(vul);
+            return sb.toString();
+        }
+        else{
+            return String.valueOf(tmp/(double) denom);
+        }
 
-        //String window =
-
-        return s;
     }
 }
